ea3c1f8cac05810dcaef2a0d424fb7ef9367862d
Adam Lenger <adam.lenger@guidevision.cz>
2022-08-19 16:55:12 +0200

    Exercise 3: Accessibility issues

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -21,9 +21,9 @@ const Hero = () => {
             /images/hero-img@3x.jpg 3x,
           "
         />
-        <HeroImage src="/images/hero-img.jpg" />
+        <HeroImage src="/images/hero-img.jpg" alt="Highlighted image"/>
       </picture>
-      <Swoop src="/swoop.svg" />
+      <Swoop src="/swoop.svg" alt=""/>
     </Wrapper>
   );
 };
